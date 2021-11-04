56ae17d4d2887c75986a0fe024752afde8b395d0
César Augusto  Montalván Morales <52120288+CescY2k@users.noreply.github.com>
2021-11-04 14:34:22 -0600

    Update settings.py

--- deployd/settings.py
+++ deployd/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import environ
+import os 
 
 root = environ.Path(start=__file__) - 2
 env = environ.Env()
